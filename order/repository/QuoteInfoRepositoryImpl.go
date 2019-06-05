6e1e72a0dc4242fd8d95722837890468711dc384
ss0311 <soichiroseike@gmail.com>
2019-06-05 22:28:41 +0900

    orderサブシステムがクオート情報を初期ロードするように実装を修正

--- order/repository/QuoteInfoRepositoryImpl.go
+++ order/repository/QuoteInfoRepositoryImpl.go
@@ -9,10 +9,24 @@ import (
 )
 
 type quoteInfoRepositoryImpl struct {
+	cache map[string][]domain.QuoteInfo
 }
 
 func NewQuoteInfoRepositoryImpl() *quoteInfoRepositoryImpl {
 	repository := new(quoteInfoRepositoryImpl)
+	repository.cache = map[string][]domain.QuoteInfo{}
+	//TODO: 初期ロードする対象の銘柄情報をどこかに記録しておく。今はベタ書き。
+	resp, err := http.Get("http://localhost:8091/quote/all/BTC/USD")
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	infoMap := map[int]dto.QuoteInfoDto{}
+	json.NewDecoder(resp.Body).Decode(&infoMap)
+	for _, v := range infoMap {
+		repository.cache[v.Side] = append(repository.cache[v.Side], domain.NewQuoteInfo(v))
+	}
 	return repository
 }
 
